5d00109c33b58d48c37d16963ba40c92183cb3ed
Ratovonirina <eric.ratovonirina@gmail.com>
2024-03-16 15:37:08 +0300

    Ajout CustomerManager

---
+++ src/main/java/service/CustomerManager.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import java.util.List;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import jakarta.persistence.Query;
+import mg.eric.tpcustomerratovonirina47.entity.Customer;
+
+/**
+ * Façade pour gérer les Customers.
+ * @author Ratovonirina
+ */
+@RequestScoped
+public class CustomerManager {
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    @Transactional
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    @Transactional
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+    
+}
